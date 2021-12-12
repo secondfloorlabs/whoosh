c872c0c609b2251cee162e461087860952fe35db
Devin Kopp <devinkopp@gwu.edu>
2021-12-12 16:24:14 -0400

    Get SPL token balances

--- src/components/accounts/Solana.tsx
+++ src/components/accounts/Solana.tsx
@@ -8,6 +8,16 @@ import { useDispatch } from 'react-redux';
 import { getWalletBalanceUSD } from 'src/utils/helpers';
 import { connect } from 'http2';
 
+interface SplToken {
+  publicKey: string;
+  ticker: string;
+}
+
+const splTokens = [
+  { publicKey: 'GuPGtixpwQTPyN7xHyyT3TMvH1dsir248GQSoTxaAMMs', ticker: 'IN' },
+  { publicKey: '9c98UD5dRCSJLk381yo9JNqn5fhiLpYnePE17tAucTP6', ticker: 'USDC' },
+];
+
 const getSolanaPrice = async () => {
   const response = await axios.get(
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=solana'
@@ -37,15 +47,28 @@ const Solana = () => {
       const balance = await connection.getBalance(address);
 
       const inToken = await connection.getTokenAccountsByOwner(address, {
-        "programId": new solanaWeb3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
-      },);
+        programId: new solanaWeb3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'),
+      });
 
       // const splToken = await connection.getTokenAccountBalance(new solanaWeb3.PublicKey("3pNHfhH31Ch7uCe5DCHj3EmHUWS2bZziUSZcP6CByZqF"));
       // console.log(splToken);
 
+      console.log(inToken);
       console.log(inToken.value[1].pubkey.toString());
 
-      console.log(await connection.getTokenAccountBalance(inToken.value[1].pubkey));
+      const tokens = [];
+      await Promise.all(
+        inToken.value.map(async (token) => {
+          const tokenKey = token.pubkey;
+          const value = (await connection.getTokenAccountBalance(tokenKey)).value;
+          console.log(tokenKey.toString());
+          if (value.amount !== '0' && value.decimals !== 0) {
+            const balance = parseFloat(value.amount) / 10 ** value.decimals;
+            tokens.push({ balance: balance, tokenKey });
+            console.log(balance);
+          }
+        })
+      );
 
       const sol = balance * 0.000000001;
       let coinPrice;
