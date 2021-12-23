6edf0ac446b85b0d34cead78f6ca88a903200822
sshah98 <shahsuraj261@gmail.com>
2021-12-22 22:18:44 -0500

    remove comment

--- src/services/coinbaseTypes.ts
+++ src/services/coinbaseTypes.ts
@@ -88,12 +88,6 @@ export interface CoinbaseTransactions {
       created_at: string;
       updated_at: string;
       resource: string;
-      // resource_path: '/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/transactions/4117f7d6-5694-5b36-bc8f-847509850ea4';
-      // buy: {
-      //   id: '9e14d574-30fa-5d85-b02c-6be0d851d61d';
-      //   resource: 'buy';
-      //   resource_path: '/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/buys/9e14d574-30fa-5d85-b02c-6be0d851d61d';
-      // };
       details: {
         title: string;
         subtitle: string;
@@ -118,12 +112,6 @@ export interface CoinbaseTransactionsComplete {
   created_at: string;
   updated_at: string;
   resource: string;
-  // resource_path: '/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/transactions/4117f7d6-5694-5b36-bc8f-847509850ea4';
-  // buy: {
-  //   id: '9e14d574-30fa-5d85-b02c-6be0d851d61d';
-  //   resource: 'buy';
-  //   resource_path: '/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/buys/9e14d574-30fa-5d85-b02c-6be0d851d61d';
-  // };
   details: {
     title: string;
     subtitle: string;
