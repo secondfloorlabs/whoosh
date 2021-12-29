87d5514aff635534838c6321bc134c84d6b73f64
sshah98 <shahsuraj261@gmail.com>
2021-12-29 09:40:24 -0500

    fix mobile

--- src/components/WhooshNavbar.tsx
+++ src/components/WhooshNavbar.tsx
@@ -9,26 +9,24 @@ const WhooshNavbar = () => {
   const user = useContext(AuthContext);
 
   return (
-    <Container>
-      <Navbar>
-        <Container>
-          <Navbar.Brand style={{ color: 'white' }} href="/">
-            <span style={{ fontFamily: 'Damion', fontSize: '200%' }}> whoosh </span> &nbsp; crypto
-            portfolio tracker
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            {!user ? (
-              <Button size="sm" variant="outline-success" onClick={logIn}>
-                Sign in
-              </Button>
-            ) : (
-              <Navbar.Text style={{ color: 'white', fontSize: 'smaller' }}>
-                {translations.welcome}
-                {`, ${user.displayName?.split(' ')[0]}`}
-              </Navbar.Text>
-            )}
-          </Navbar.Collapse>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand style={{ color: 'white' }} href="/">
+          <span style={{ fontFamily: 'Damion', fontSize: '200%' }}> whoosh </span> &nbsp; crypto
+          portfolio tracker
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          {!user ? (
+            <Button size="sm" variant="outline-success" onClick={logIn}>
+              Sign in
+            </Button>
+          ) : (
+            <Navbar.Text style={{ color: 'white', fontSize: 'smaller' }}>
+              {translations.welcome}
+              {`, ${user.displayName?.split(' ')[0]}`}
+            </Navbar.Text>
+          )}
           {!isProduction() && user && (
             <Nav.Link>
               <Button size="sm" onClick={logOut}>
@@ -36,9 +34,9 @@ const WhooshNavbar = () => {
               </Button>
             </Nav.Link>
           )}
-        </Container>
-      </Navbar>
-    </Container>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
