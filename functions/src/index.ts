52695673a7a554c97be8acd19d1cd1be1aeeb91a
Suraj Shah <shahsuraj261@gmail.com>
2021-12-29 20:05:47 -0500

    (wip) Gemini (#36)
    * WIP for gemini
    
    * see if gemini works'

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import cors = require('cors');
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import crypto = require('crypto');
 
 const app = express();
@@ -41,7 +41,7 @@ app.get('/coinbaseProAccounts', async (req, res) => {
     });
     return res.status(200).json(response.data);
   } catch (err) {
-    return res.status(400).json({ error: err });
+    return res.status(400).json({ err });
   }
 });
 
@@ -76,7 +76,7 @@ app.get('/coinbaseProLedger', async (req, res) => {
     });
     return res.status(200).json(response.data);
   } catch (err) {
-    return res.status(400).json({ error: err });
+    return res.status(400).json({ err });
   }
 });
 
@@ -97,6 +97,67 @@ app.get('/geminiAuth', async (req, res) => {
     });
 
     return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(400).json({ err });
+  }
+});
+
+app.get('/geminiRefresh', async (req, res) => {
+  const { refresh_token } = req.query;
+
+  const query = `https://exchange.gemini.com/auth/token`;
+  const client_id = '61c7adff-a5df-4dad-8dbc-63ac58372dc5';
+  const client_secret = '61c7adff-f7e6-493f-bbf9-9c25240a8e65';
+
+  try {
+    const response = await axios.post(query, {
+      grant_type: 'refresh_token',
+      refresh_token,
+      client_id,
+      client_secret,
+    });
+
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(400).json({ err });
+  }
+});
+
+app.get('/geminiAccounts', async (req, res) => {
+  const { access_token } = req.query;
+
+  const geminiBaseUrl = 'https://api.gemini.com';
+
+  const balancePath = '/v1/notionalbalances/usd';
+  const earnPath = '/v1/balances/earn';
+
+  const balanceQuery = geminiBaseUrl + balancePath;
+  const earnQuery = geminiBaseUrl + earnPath;
+
+  const balancePayload = { request: balancePath };
+  const earnPayload = { request: earnPath };
+
+  const balancePayloadEncoded = Buffer.from(JSON.stringify(balancePayload)).toString('base64');
+  const earnPayloadEncoded = Buffer.from(JSON.stringify(earnPayload)).toString('base64');
+  const balanceHeaders = {
+    Authorization: `Bearer ${access_token}`,
+    'X-GEMINI-PAYLOAD': String(balancePayloadEncoded),
+  };
+
+  const earnHeaders = {
+    Authorization: `Bearer ${access_token}`,
+    'X-GEMINI-PAYLOAD': String(earnPayloadEncoded),
+  };
+
+  try {
+    const [balanceResponse, earnResponse] = await Promise.all([
+      axios.post(balanceQuery, {}, { headers: balanceHeaders }),
+      axios.post(earnQuery, {}, { headers: earnHeaders }),
+    ]);
+
+    const balance = balanceResponse.data.map((b: any) => ({ ...b, type: 'Balance' }));
+
+    return res.status(200).json([...balance, ...earnResponse.data]);
   } catch (err) {
     return res.status(400).json({ err });
   }
--- src/components/accounts/Gemini.tsx
+++ src/components/accounts/Gemini.tsx
@@ -1,16 +1,21 @@
 import { useContext, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-// import { WALLETS } from 'src/utils/constants';
-// import { getCoinPriceFromName } from 'src/utils/prices';
-// import * as actionTypes from 'src/store/actionTypes';
-import { authCodeAccess, createGeminiUrl, storeTokensLocally } from 'src/services/gemini';
+import * as actionTypes from 'src/store/actionTypes';
+import {
+  accessAccount,
+  authCodeAccess,
+  createGeminiUrl,
+  refreshTokenAccess,
+  storeTokensLocally,
+} from 'src/services/gemini';
 import { captureMessage } from '@sentry/react';
 import { Button } from 'react-bootstrap';
 import { AuthContext } from 'src/context/AuthContext';
 import { addUserAccessData } from 'src/services/firebase';
-
-// https://docs.gemini.com/oauth/#using-access-tokens
+import { Balance, Earn, WalletType } from 'src/interfaces/gemini';
+import { WALLETS } from 'src/utils/constants';
+import { getCoinPriceFromName } from 'src/utils/prices';
 
 const Gemini = () => {
   const dispatch = useDispatch();
@@ -28,6 +33,47 @@ const Gemini = () => {
   }, [user]);
 
   useEffect(() => {
+    const getWalletData = async (wallets: Balance[] | Earn[]) => {
+      await Promise.all(
+        wallets.map(async (w) => {
+          let token: IToken;
+
+          const symbol = w.currency;
+
+          const rawHistoricalPrices = await getCoinPriceFromName(symbol, symbol);
+
+          const currentPrice = rawHistoricalPrices[rawHistoricalPrices.length - 1][1];
+          const lastPrice = rawHistoricalPrices[rawHistoricalPrices.length - 2][1];
+
+          if (w.type === WalletType.BALANCE) {
+            const wallet = w as Balance;
+
+            token = {
+              walletName: WALLETS.GEMINI,
+              name: wallet.currency,
+              balance: +wallet.amount,
+              symbol,
+              price: currentPrice,
+              lastPrice,
+            };
+          } else {
+            const wallet = w as Earn;
+            token = {
+              walletName: WALLETS.GEMINI,
+              name: wallet.currency,
+              balance: wallet.balance,
+              symbol,
+              price: currentPrice,
+              lastPrice,
+            };
+          }
+
+          dispatch({ type: actionTypes.ADD_ALL_TOKEN, token });
+          dispatch({ type: actionTypes.ADD_CURRENT_TOKEN, token });
+        })
+      );
+    };
+
     const geminiInitialAuth = async () => {
       const search = window.location.search;
       const params = new URLSearchParams(search);
@@ -38,19 +84,42 @@ const Gemini = () => {
       try {
         const geminiAccess = await authCodeAccess(code);
 
-        // const accessToken = geminiAccess.access_token;
+        const accessToken = geminiAccess.access_token;
         storeTokensLocally(geminiAccess);
 
-        // const geminiAccount = await accessAccount(accessToken);
-        // const wallets = geminiAccount.reverse(); // primary wallet (BTC) top of list
-        // getWalletData(wallets);
+        const geminiAccount = await accessAccount(accessToken);
+        getWalletData(geminiAccount);
+
         setAuthorized(true);
       } catch (err) {
         captureMessage(`Invalid gemini param code\n${err}`);
       }
     };
 
-    const geminiReauth = async () => {};
+    const geminiReauth = async () => {
+      try {
+        const accountLocal = await accessAccount(localStorage.getItem('geminiAccessToken'));
+        getWalletData(accountLocal);
+        setAuthorized(true);
+      } catch (err) {
+        // access token failed
+        try {
+          const tokenAccess = await refreshTokenAccess(localStorage.getItem('geminiRefreshToken'));
+
+          // refresh local storage
+          const accessToken = tokenAccess.access_token;
+          storeTokensLocally(tokenAccess);
+
+          const geminiAccount = await accessAccount(accessToken);
+          getWalletData(geminiAccount);
+
+          setAuthorized(true);
+        } catch (err) {
+          // refresh and access token failed
+          geminiInitialAuth();
+        }
+      }
+    };
 
     if (localStorage.getItem('geminiAccessToken') === null) {
       // first time auth
