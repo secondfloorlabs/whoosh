5bf979130ef69a5d8d5cd5777af590af279ed4f4
SGtheLucky1 <sglucky1@gmail.com>
2022-01-07 04:36:33 -0500

    Added mixpanel, added redux devtools integration for debugging redux

---
+++ src/utils/mixpanel.ts
@@ -0,0 +1,23 @@
+import mixpanel from "mixpanel-browser";
+mixpanel.init("17a9c18890a5cf5af8a5e5ffe3763ce8");
+
+let env_check = true //process.env.NODE_ENV === "production";
+
+let actions = {
+    identify: (id?: string) => {
+        if (env_check) mixpanel.identify(id);
+    },
+    alias: (id: string) => {
+        if (env_check) mixpanel.alias(id);
+    },
+    track: (name: string, props?: {}) => {
+        if (env_check) mixpanel.track(name, props);
+    },
+    people: {
+        set: (props: {}) => {
+            if (env_check) mixpanel.people.set(props);
+        },
+    },
+};
+
+export let Mixpanel = actions;
