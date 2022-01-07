b1ff644cd725807abb0ed395028daaa48e4c6d2f
Devin Kopp <devinkopp@gwu.edu>
2022-01-07 14:53:22 -0500

    Remove x axis, add date label to tooltip, scale y axis (#60)
    * Remove x axis, add date label to tooltip, scale y axis
    
    * Pad bottom of graph scaled by distance to max

--- src/components/NetWorthGraph.tsx
+++ src/components/NetWorthGraph.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { XAxis, Tooltip, ResponsiveContainer, AreaChart, Area } from 'recharts';
+import { XAxis, Tooltip, ResponsiveContainer, AreaChart, Area, YAxis } from 'recharts';
 import { displayInUSD } from 'src/utils/helpers';
 import Loading from 'src/components/Loading';
 import { isMobile } from 'react-device-detect';
@@ -17,6 +17,8 @@ interface NetWorthGraphProps {
 
 export default function NetWorthGraph(props: NetWorthGraphProps) {
   const [graphData, setGraphData] = useState<DataPoint[]>([]);
+  const [netWorthMin, setNetWorthMin] = useState<number>(0);
+  const [netWorthMax, setNetWorthMax] = useState<number>(0);
   const tokens = useSelector<TokenState, TokenState['allTokens']>((state) => state.allTokens);
 
   useEffect(() => {
@@ -41,8 +43,15 @@ export default function NetWorthGraph(props: NetWorthGraphProps) {
       newGraphData.push({ timestamp: +timestamp, worth });
     }
     newGraphData.push({ timestamp: Date.now() / 1000, worth: props.currentBalance });
+
+    const worths = newGraphData.map((dataPoint) => dataPoint.worth);
+    const min = Math.min(...worths);
+    const max = Math.max(...worths);
+    setNetWorthMin(min);
+    setNetWorthMax(max);
+
     setGraphData(newGraphData);
-  }, [tokens, props.currentBalance]);
+  }, [setNetWorthMin, setNetWorthMax, tokens, props.currentBalance]);
 
   return (
     <div className={isMobile ? 'portfolioChartMobile1' : 'portfolioChart1'}>
@@ -50,24 +59,29 @@ export default function NetWorthGraph(props: NetWorthGraphProps) {
         <Loading text={'Graph Loading...'} />
       ) : (
         <ResponsiveContainer width="100%" height="100%">
-          <AreaChart height={380} data={graphData}>
+          <AreaChart margin={{ left: -5, right: -5 }} height={380} data={graphData}>
             <defs>
-              <linearGradient id="netWorth" x1="0" y1="0" x2="0" y2="15">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.1} />
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              <linearGradient id="netWorth" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#8884d8" stopOpacity={0.5} />
+                <stop offset="100%" stopColor="#8884d8" stopOpacity={0} />
               </linearGradient>
             </defs>
-
-            <XAxis
-              dataKey="timestamp"
-              tickFormatter={(value, index) => {
-                return new Date(value * 1000).toLocaleDateString();
-              }}
+            <XAxis hide dataKey="timestamp" />
+            <YAxis
+              hide
+              dataKey="worth"
+              domain={[netWorthMin - (netWorthMax - netWorthMin) / 3, netWorthMax * 1.02]}
             />
             <Tooltip
+              itemStyle={{ backgroundColor: '#181A1B' }}
+              wrapperStyle={{ backgroundColor: '#181A1B' }}
+              contentStyle={{ backgroundColor: '#181A1B' }}
               viewBox={{ x: 0, y: 0, width: 100, height: 100 }}
               formatter={(value: any) => {
                 return [`${displayInUSD(value)}`, 'Net Worth'];
+              }}
+              labelFormatter={(timestamp) => {
+                return new Date(timestamp * 1000).toLocaleDateString();
               }}
             />
             <Area
