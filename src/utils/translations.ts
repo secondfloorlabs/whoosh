c42a7a8972599700e5b9dfeb2a66558a681f967e
sshah98 <shahsuraj261@gmail.com>
2022-01-12 07:31:55 -0800

    feedback

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,4 +1,4 @@
 export const noPriceFound = 'N/A';
 export const walletsExchanges = 'Wallets + Exchanges';
 export const welcome = 'Welcome';
-export const noProfitValue = 'No P/L ratio';
+export const noProfitValue = 'P/L Coming Soon';
