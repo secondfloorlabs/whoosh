c0cb3c0e885395931b7116dff545cd6942121e1d
Devin Kopp <devinkopp@gwu.edu>
2022-01-13 16:36:20 -0500

    Dedupe based only on ticker (#69)

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,7 +1,3 @@
 export function isSameToken(token1: IToken, token2: IToken): boolean {
-  return (
-    token1.name.toLowerCase() === token2.name.toLowerCase() &&
-    token1.symbol.toLowerCase() === token2.symbol.toLowerCase() &&
-    token1.network?.toLowerCase() === token2.network?.toLowerCase()
-  );
+  return token1.symbol.toLowerCase() === token2.symbol.toLowerCase();
 }
